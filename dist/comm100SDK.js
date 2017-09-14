503c832ee8c5728a08eae4a363c48d56a67556a0
evan <evan@comm100.com>
2017-09-14 15:56:43 +0800

    去掉中文注释

--- dist/comm100SDK.js
+++ dist/comm100SDK.js
@@ -48,197 +48,225 @@ var index =
 	'use strict';
 	
 	Object.defineProperty(exports, "__esModule", {
-	    value: true
+	  value: true
 	});
 	/**
 	 * @module Comm100AgentConsoleAPI
-	 * Comm100AgentConsoleAPI AgentConsole提供的SDK库调用封装
+	 * Comm100AgentConsoleAPI AgentConsole
 	 * @method Comm100AgentConsoleAPI
 	 * @author yanglang
 	 * @date 20170830
 	 */
 	
-	var Events = {},
-	    eventName = '',
-	    toBeNotify = [],
-	    EVENT_PREFIX = 'TPE'; //临时事件名称前缀，后缀为_+时间缀
-	
+	var Events = {};
+	var toBeNotify = [];
+	var EVENT_PREFIX = 'TPE';
 	
 	var _ = {
-	    /**
-	     * 触发一个事件
-	     * @method notify
-	     * @param eventName 事件名称
-	     * @param data 事件数据 PS：现在支持变参，除了eventName,data以外还可以添加任意参数
-	     * @returns {_}
-	     */
-	    notify: function notify(eventName, data) {
-	        var eventList = Events[eventName],
-	            i = 0;
-	        if (eventList) {
-	            var len = eventList.length;
-	            for (; i < len; i++) {
-	                eventList[i].apply(this, toBeNotify.slice.call(arguments, 1));
-	            }
-	        } else {
-	            toBeNotify.push({
-	                eventName: eventName,
-	                data: toBeNotify.slice.call(arguments, 1),
-	                scope: this
-	            }); //暂时存入待触发列表
-	        }
-	        //若为临时事件，则通知一次之后马上注销
-	        if (eventName.startsWith(EVENT_PREFIX + '_')) this.unsubscribe(eventName);
-	        return this;
-	    },
+	  /*
+	   * @method notify
+	   * @param eventName
+	   * @returns {_}
+	   */
+	  notify: function notify(eventName) {
+	    var eventList = Events[eventName];
+	    var i = 0;
 	
-	    /**
-	     * 给定作用域触发一个事件
-	     * @param eventName 事件名称
-	     * @param scope 作用域
-	     * @param data 事件数据，支持变参
-	     */
-	    notifyWith: function notifyWith(eventName, scope, data) {
-	        if (arguments.length < 2) throw new TypeError('按作用域触发事件请提供事件名称与作用域');
-	        this.notify.apply(scope, [eventName].concat(toBeNotify.slice.call(arguments, 2)));
-	    },
-	
-	    /**
-	     * 订阅一个事件
-	     * @method subscribe
-	     * @param eventName 事件名称
-	     * @param callback 事件回调
-	     */
-	    subscribe: function subscribe(eventName, callback) {
-	        var i = 0,
-	            len = toBeNotify.length;
-	        if (arguments.length < 2) throw new TypeError('订阅事件请提供事件名称与事件回调');
-	
-	        var eventList = Events[eventName] ? Events[eventName] : Events[eventName] = [];
-	        eventList = Object.prototype.toString.call(callback) === '[object Array]' ? eventList.concat(callback) : eventList.push(callback);
-	        for (; i < len; i++) {
-	            if (toBeNotify[i].eventName === eventName) {
-	                //移除并触发之前已准备触发的事件
-	                this.notify.apply(toBeNotify[i].scope, [eventName].concat(toBeNotify[i].data));
-	                toBeNotify.splice(i, 1);
-	                break;
-	            }
-	        }
-	        return this;
-	    },
-	
-	    /**
-	     * 取消订阅事件
-	     * @method unsubscribe
-	     * @param eventName 事件名称
-	     */
-	    unsubscribe: function unsubscribe(eventName, callback) {
-	        if (callback) {
-	            var callbacks = Events[eventName];
-	            for (var i = 0; i < callbacks.length; i++) {
-	                if (callbacks[i] === callback) {
-	                    callbacks.splice(i--, 1);
-	                }
-	            }
-	        } else delete Events[eventName];
-	        return this;
-	    },
-	    guid: function guid() {
-	        return 'xxxxxxxx_xxxx_4xxx_yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-	            var r = Math.random() * 16 | 0,
-	                v = c == 'x' ? r : r & 0x3 | 0x8;
-	            return v.toString(16);
-	        });
-	    },
-	
-	    /**
-	     * 调用相关接口
-	     * @method call
-	     * @param api 请求地址
-	     * @param data Object 
-	     * @param alive 非临时事件
-	     */
-	    call: function call(api, data, alive) {
-	        if (data.callback) {
-	            //非临时事件使用api作为事件名称
-	            eventName = !alive ? EVENT_PREFIX + '_' + this.guid() : api;
-	            this.subscribe(eventName, data.callback);
-	        }
-	        var messageObj = {
-	            api: api,
-	            action: data.action,
-	            params: data.params,
-	            msgId: eventName
-	        };
-	        var message = JSON.stringify(messageObj);
-	        window && window.parent && window.parent.postMessage(message, '*'); //TODO 暂时写死* 界时改为comm100服务器地址
-	
-	        console.log('call', message);
-	        return this;
+	    for (var _len = arguments.length, rest = Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
+	      rest[_key - 1] = arguments[_key];
 	    }
+	
+	    if (eventList) {
+	      var len = eventList.length;
+	      for (; i < len; i += 1) {
+	        var _toBeNotify$slice;
+	
+	        eventList[i].apply(this, (_toBeNotify$slice = toBeNotify.slice).call.apply(_toBeNotify$slice, rest.concat([1])));
+	      }
+	    } else {
+	      var _toBeNotify$slice2;
+	
+	      toBeNotify.push({
+	        eventName: eventName,
+	        data: (_toBeNotify$slice2 = toBeNotify.slice).call.apply(_toBeNotify$slice2, rest.concat([1])),
+	        scope: this
+	      });
+	    }
+	    if (eventName.startsWith(EVENT_PREFIX + '_')) {
+	      this.unsubscribe(eventName);
+	    }
+	    return this;
+	  },
+	
+	  /*
+	   * @param eventName
+	   * @param scope
+	   * @param data
+	   */
+	  notifyWith: function notifyWith(eventName, scope) {
+	    for (var _len2 = arguments.length, rest = Array(_len2 > 2 ? _len2 - 2 : 0), _key2 = 2; _key2 < _len2; _key2++) {
+	      rest[_key2 - 2] = arguments[_key2];
+	    }
+	
+	    var _toBeNotify$slice3;
+	
+	    if (arguments.length < 2) {
+	      throw new TypeError('arguments error');
+	    }
+	    this.notify.apply(scope, [eventName].concat((_toBeNotify$slice3 = toBeNotify.slice).call.apply(_toBeNotify$slice3, rest.concat([2]))));
+	  },
+	
+	  /*
+	   * @method subscribe
+	   * @param eventName
+	   * @param callback
+	   */
+	  subscribe: function subscribe(eventName, callback) {
+	    var i = 0;
+	    var len = toBeNotify.length;
+	    if (arguments.length < 2) {
+	      throw new TypeError('arguments error ');
+	    }
+	
+	    var eventList = Events[eventName] ? Events[eventName] : Events[eventName] = [];
+	    if (Object.prototype.toString.call(callback) === '[object Array]') {
+	      eventList = eventList.concat(callback);
+	    } else {
+	      eventList.push(callback);
+	    }
+	    for (; i < len; i += 1) {
+	      if (toBeNotify[i].eventName === eventName) {
+	        this.notify.apply(toBeNotify[i].scope, [eventName].concat(toBeNotify[i].data));
+	        toBeNotify.splice(i, 1);
+	        break;
+	      }
+	    }
+	    return this;
+	  },
+	
+	  /*
+	   * @method unsubscribe
+	   * @param eventName
+	   */
+	  unsubscribe: function unsubscribe(eventName, callback) {
+	    if (callback) {
+	      var callbacks = Events[eventName];
+	      for (var i = 0; i < callbacks.length; i += 1) {
+	        if (callbacks[i] === callback) {
+	          callbacks.splice(i -= 1, 1);
+	        }
+	      }
+	    } else {
+	      delete Events[eventName];
+	    }
+	    return this;
+	  },
+	  guid: function guid() {
+	    return 'xxxxxxxx_xxxx_4xxx_yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+	      /* eslint-disable */
+	      var r = Math.random() * 16 | 0;
+	      var v = c === 'x' ? r : r & 0x3 | 0x8;
+	      /* eslint-enable */
+	      return v.toString(16);
+	    });
+	  },
+	
+	  /*
+	   * @method call
+	   * @param api
+	   * @param data Object
+	   * @param alive
+	   */
+	  call: function call(api, data, alive) {
+	    var eventName = '';
+	    if (data.callback) {
+	      eventName = !alive ? EVENT_PREFIX + '_' + this.guid() : api;
+	      this.subscribe(eventName, data.callback);
+	    }
+	    var messageObj = {
+	      api: api,
+	      action: data.action,
+	      params: data.params,
+	      msgId: eventName
+	    };
+	    var message = JSON.stringify(messageObj);
+	
+	    if (typeof window !== 'undefined') {
+	      if (typeof window.parent !== 'undefined') {
+	        window.parent.postMessage(message, '*');
+	      }
+	    }
+	
+	    console.log('call', message);
+	    return this;
+	  }
 	};
 	
 	var Comm100AgentConsoleAPI = {
-	    onReady: function onReady(callback) {
-	        window.addEventListener('message', function (e) {
-	            if (e.source != window.parent) {
-	                console.log('我是自己页面的消息，不予处理，返回');
-	                return;
-	            }
-	            var data = JSON.parse(e.data);
-	            _.notify(data.msgId || data.api, data);
-	        }, false);
-	        callback && callback();
-	    },
-	    init: function init() {},
-	    get: function get(key) {
-	        return new Promise(function (resolve, reject) {
-	            _.call(key, {
-	                action: 'get',
-	                callback: function callback(data) {
-	                    resolve(data);
-	                    console.log('resolve', data);
-	                }
-	            });
-	        });
-	    },
-	    set: function set(key, value) {
-	        return new Promise(function (resolve, reject) {
-	            _.call(key, {
-	                action: 'set',
-	                params: {
-	                    value: value
-	                },
-	                callback: function callback(data) {
-	                    resolve(data);
-	                    console.log('resolve', data);
-	                }
-	            });
-	        });
-	    },
-	    on: function on(key, callback) {
-	        _.call(key, {
-	            action: 'on',
-	            callback: callback
-	        }, true);
-	    },
-	    do: function _do(key, value) {
-	        return new Promise(function (resolve, reject) {
-	            _.call(key, {
-	                action: 'do',
-	                params: {
-	                    value: encodeURIComponent(value)
-	                },
-	                callback: function callback(data) {
-	                    resolve(data);
-	                    console.log('resolve', data);
-	                }
-	            });
-	        });
+	  onReady: function onReady(callback) {
+	    window.addEventListener('message', function (e) {
+	      if (e.source !== window.parent) {
+	        console.log('from self return');
+	        return;
+	      }
+	      var data = JSON.parse(e.data);
+	      _.notify(data.msgId || data.api, data);
+	    }, false);
+	    if (callback) {
+	      callback();
 	    }
+	  },
+	  init: function init() {},
+	  get: function get(key) {
+	    return new Promise(function (resolve) {
+	      _.call(key, {
+	        action: 'get',
+	        callback: function callback(data) {
+	          resolve(data);
+	          console.log('resolve', data);
+	        }
+	      });
+	    });
+	  },
+	  set: function set(key, value) {
+	    return new Promise(function (resolve) {
+	      _.call(key, {
+	        action: 'set',
+	        params: {
+	          value: value
+	        },
+	        callback: function callback(data) {
+	          resolve(data);
+	          console.log('resolve', data);
+	        }
+	      });
+	    });
+	  },
+	  on: function on(key, callback) {
+	    _.call(key, {
+	      action: 'on',
+	      callback: callback
+	    }, true);
+	  },
+	  do: function _do(key, value) {
+	    return new Promise(function (resolve) {
+	      _.call(key, {
+	        action: 'do',
+	        params: {
+	          value: encodeURIComponent(value)
+	        },
+	        callback: function callback(data) {
+	          resolve(data);
+	          console.log('resolve', data);
+	        }
+	      });
+	    });
+	  }
 	};
 	
-	window && (window.Comm100AgentConsoleAPI = Comm100AgentConsoleAPI);
+	if (typeof window !== 'undefined') {
+	  window.Comm100AgentConsoleAPI = Comm100AgentConsoleAPI;
+	}
 	
 	exports.default = Comm100AgentConsoleAPI;
 
--- src/index.es6
+++ src/index.es6
@@ -6,180 +6,191 @@
  * @date 20170830
  */
 
-let Events = {},
-    eventName = '',
-    toBeNotify = [],
-    EVENT_PREFIX = 'TPE'; 
+const Events = {};
+const toBeNotify = [];
+const EVENT_PREFIX = 'TPE';
 
 
 const _ = {
-    /**
-     * @method notify
-     * @param eventName 
-     * @returns {_}
-     */
-    notify(eventName, data) {
-        var eventList = Events[eventName],
-            i = 0;
-        if (eventList) {
-            var len = eventList.length;
-            for (; i < len; i++) {
-                eventList[i].apply(this, toBeNotify.slice.call(arguments, 1));
-            }
-        } else {
-            toBeNotify.push({
-                eventName: eventName,
-                data: toBeNotify.slice.call(arguments, 1),
-                scope: this
-            });
-        }
-        if (eventName.startsWith(EVENT_PREFIX + '_'))
-            this.unsubscribe(eventName);
-        return this;
-    },
-    /**
-     * @param eventName 
-     * @param scope 
-     * @param data 
-     */
-    notifyWith(eventName, scope, data) {
-        if (arguments.length < 2)
-            throw new TypeError('arguments error');
-        this.notify.apply(scope, [eventName].concat(toBeNotify.slice.call(arguments, 2)));
-    },
-    /**
-     * @method subscribe
-     * @param eventName 
-     * @param callback 
-     */
-    subscribe(eventName, callback) {
-        var i = 0,
-            len = toBeNotify.length;
-        if (arguments.length < 2)
-            throw new TypeError('arguments error ');
-
-        var eventList = Events[eventName] ? Events[eventName] : (Events[eventName] = []);
-        eventList = Object.prototype.toString.call(callback) === '[object Array]' ? eventList.concat(callback) : eventList.push(callback);
-        for (; i < len; i++) {
-            if (toBeNotify[i].eventName === eventName) {
-                this.notify.apply(toBeNotify[i].scope, [eventName].concat(toBeNotify[i].data));
-                toBeNotify.splice(i, 1);
-                break;
-            }
-        }
-        return this;
-    },
-    /**
-     * @method unsubscribe
-     * @param eventName 
-     */
-    unsubscribe(eventName, callback) {
-        if (callback) {
-            var callbacks = Events[eventName];
-            for (var i = 0; i < callbacks.length; i++) {
-                if (callbacks[i] === callback) {
-                    callbacks.splice(i--, 1);
-                }
-            }
-        } else
-            delete Events[eventName];
-        return this;
-    },
-    guid() {
-        return 'xxxxxxxx_xxxx_4xxx_yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c)=> {
-            var r = Math.random() * 16 | 0,
-                v = c == 'x' ? r : (r & 0x3 | 0x8);
-            return v.toString(16);
-        });
-    },
-    /**
-     * @method call
-     * @param api 
-     * @param data Object 
-     * @param alive 
-     */
-    call(api, data, alive) {
-        if (data.callback) {
-            eventName = !alive ? EVENT_PREFIX + '_' + this.guid() : api;
-            this.subscribe(eventName, data.callback);
-        }
-        let messageObj = {
-            api: api,
-            action: data.action,
-            params: data.params,
-            msgId: eventName
-        };
-        let message = JSON.stringify(messageObj);
-        window && window.parent && window.parent.postMessage(message, '*');
-
-        console.log('call', message)
-        return this;
+  /*
+   * @method notify
+   * @param eventName
+   * @returns {_}
+   */
+  notify(eventName, ...rest) {
+    const eventList = Events[eventName];
+    let i = 0;
+    if (eventList) {
+      const len = eventList.length;
+      for (; i < len; i += 1) {
+        eventList[i].apply(this, toBeNotify.slice.call(...rest, 1));
+      }
+    } else {
+      toBeNotify.push({
+        eventName,
+        data: toBeNotify.slice.call(...rest, 1),
+        scope: this,
+      });
     }
+    if (eventName.startsWith(`${EVENT_PREFIX}_`)) { this.unsubscribe(eventName); }
+    return this;
+  },
+  /*
+   * @param eventName
+   * @param scope
+   * @param data
+   */
+  notifyWith(eventName, scope, ...rest) {
+    if (arguments.length < 2) { throw new TypeError('arguments error'); }
+    this.notify.apply(scope, [eventName].concat(toBeNotify.slice.call(...rest, 2)));
+  },
+  /*
+   * @method subscribe
+   * @param eventName
+   * @param callback
+   */
+  subscribe(eventName, callback) {
+    let i = 0;
+    const len = toBeNotify.length;
+    if (arguments.length < 2) { throw new TypeError('arguments error '); }
+
+    let eventList = Events[eventName] ? Events[eventName] : (Events[eventName] = []);
+    if (Object.prototype.toString.call(callback) === '[object Array]') {
+      eventList = eventList.concat(callback);
+    } else {
+      eventList.push(callback);
+    }
+    for (; i < len; i += 1) {
+      if (toBeNotify[i].eventName === eventName) {
+        this.notify.apply(toBeNotify[i].scope, [eventName].concat(toBeNotify[i].data));
+        toBeNotify.splice(i, 1);
+        break;
+      }
+    }
+    return this;
+  },
+  /*
+   * @method unsubscribe
+   * @param eventName
+   */
+  unsubscribe(eventName, callback) {
+    if (callback) {
+      const callbacks = Events[eventName];
+      for (let i = 0; i < callbacks.length; i += 1) {
+        if (callbacks[i] === callback) {
+          callbacks.splice(i -= 1, 1);
+        }
+      }
+    } else { delete Events[eventName]; }
+    return this;
+  },
+  guid() {
+    return 'xxxxxxxx_xxxx_4xxx_yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+      /* eslint-disable */
+      const r = Math.random() * 16 | 0;
+      const v = c === 'x' ? r : (r & 0x3 | 0x8);
+      /* eslint-enable */
+      return v.toString(16);
+    });
+  },
+  /*
+   * @method call
+   * @param api
+   * @param data Object
+   * @param alive
+   */
+  call(api, data, alive) {
+    let eventName = '';
+    if (data.callback) {
+      eventName = !alive ? `${EVENT_PREFIX}_${this.guid()}` : api;
+      this.subscribe(eventName, data.callback);
+    }
+    const messageObj = {
+      api,
+      action: data.action,
+      params: data.params,
+      msgId: eventName,
+    };
+    const message = JSON.stringify(messageObj);
+
+    if (typeof window !== 'undefined') {
+      if (typeof window.parent !== 'undefined') {
+        window.parent.postMessage(message, '*');
+      }
+    }
+
+    console.log('call', message);
+    return this;
+  },
 };
 
 
 const Comm100AgentConsoleAPI = {
-    onReady(callback) {
-        window.addEventListener('message',e=>{
-            if(e.source!=window.parent) {
-                console.log('from self return');
-                return;
-            }
-            let data = JSON.parse(e.data);
-            _.notify(data.msgId || data.api,data);
-        },false);
-        callback && callback();
-    },
-    init() {
-
-    },
-    get(key) {
-        return new Promise((resolve, reject)=> {
-            _.call(key, {
-                action: 'get',
-                callback: (data)=> {
-                    resolve(data);
-                    console.log('resolve', data);
-                }
-            });
-        });
-    },
-    set(key, value) {
-        return new Promise((resolve, reject)=> {
-            _.call(key, {
-                action: 'set',
-                params: {
-                    value: value
-                },
-                callback: (data)=> {
-                    resolve(data);
-                    console.log('resolve', data);
-                }
-            });
-        });
-    },
-    on(key, callback) {
-        _.call(key, {
-            action: 'on',
-            callback: callback
-        }, true);
-    },
-    do(key, value) {
-        return new Promise((resolve, reject) =>{
-            _.call(key, {
-                action: 'do',
-                params: {
-                    value: encodeURIComponent(value)
-                },
-                callback: (data)=> {
-                    resolve(data);
-                    console.log('resolve', data);
-                }
-            });
-        });
+  onReady(callback) {
+    window.addEventListener('message', (e) => {
+      if (e.source !== window.parent) {
+        console.log('from self return');
+        return;
+      }
+      const data = JSON.parse(e.data);
+      _.notify(data.msgId || data.api, data);
+    }, false);
+    if (callback) {
+      callback();
     }
+  },
+  init() {
+
+  },
+  get(key) {
+    return new Promise((resolve) => {
+      _.call(key, {
+        action: 'get',
+        callback: (data) => {
+          resolve(data);
+          console.log('resolve', data);
+        },
+      });
+    });
+  },
+  set(key, value) {
+    return new Promise((resolve) => {
+      _.call(key, {
+        action: 'set',
+        params: {
+          value,
+        },
+        callback: (data) => {
+          resolve(data);
+          console.log('resolve', data);
+        },
+      });
+    });
+  },
+  on(key, callback) {
+    _.call(key, {
+      action: 'on',
+      callback,
+    }, true);
+  },
+  do(key, value) {
+    return new Promise((resolve) => {
+      _.call(key, {
+        action: 'do',
+        params: {
+          value: encodeURIComponent(value),
+        },
+        callback: (data) => {
+          resolve(data);
+          console.log('resolve', data);
+        },
+      });
+    });
+  },
 };
 
-window && (window.Comm100AgentConsoleAPI = Comm100AgentConsoleAPI);
+if (typeof window !== 'undefined') {
+  window.Comm100AgentConsoleAPI = Comm100AgentConsoleAPI;
+}
 
 export default Comm100AgentConsoleAPI;
